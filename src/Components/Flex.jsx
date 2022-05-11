7c1f1ee6e854ce0690d722fb35eb32d4ecbd9016
Gayane25 <gayabennu@gmail.com>
2022-05-12 00:31:21 +0400

    feat(global style): added body style in index.js with createGlobal

--- src/Components/Flex.jsx
+++ src/Components/Flex.jsx
@@ -8,9 +8,9 @@ justify-content: ${props=>props.justify || "stretch"};
 padding: ${({padding})=>padding ||"0"};
 margin: ${({margin})=>margin || "0"};
 `;
+
 function Flex(props) {
     return <styledFlex {...props} />;
 
-    
 }
 export default Flex;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import styled, {createGlobalStyle} from "styled-components";
 
+const Global = createGlobalStyle`
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}`;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
+            <Global />
             <App />
         </BrowserRouter>
     </React.StrictMode>
