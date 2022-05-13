2107a6278e98754beb9e3b15672accf90aec71d6
armeagle777 <tyeranyan@gmail.com>
2022-05-13 10:53:12 +0400

    feat(api-create-post):possibility to create a post by sending data to  /api/posts/cretae  url

---
+++ backend/routes/posts.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const Post = require('../models/Post')
+
+
+router.post('/create', async (req, res) => {
+    try {
+        const {userId, title, description, image} = req.body
+        const post = await new Post({
+            owner: userId,
+            content: {
+                title: title,
+                description: description,
+                image: image
+            },
+        })
+        await post.save()
+        res.send('Your post was successfully added')
+    } catch (err) {
+        res.send('Check Your data and try again')
+    }
+
+})
+
+
+module.exports = router
+
